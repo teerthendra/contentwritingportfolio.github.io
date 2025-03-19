8da0d78b1244b6bde06464d610d27d818a84d736
Teerthendra Singh <156646241+teerthendra@users.noreply.github.com>
2025-03-19 11:15:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-// Interactive 3D text hover effect
-const heroText = document.querySelector('.hero-text');
-document.addEventListener('mousemove', (e) => {
-    let x = (window.innerWidth / 2 - e.pageX) / 20;
-    let y = (window.innerHeight / 2 - e.pageY) / 20;
-    heroText.style.transform = `perspective(800px) rotateX(${y}deg) rotateY(${x}deg)`;
-});
